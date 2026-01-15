2cba90db0a1e170604b9bc0f3b3a8fb9427b5485
ZaharBychkov <zahar.bychkov.gome1@gmail.com>
2026-01-15 14:26:16 +0300

    Модификация изображений, добавление иконки clock

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       // home: Scaffold(//home - начальный экран приложения
       //   body: RecipeList(recipes: RecipeManager().getRecipes()),//Основное содержимое
       // )
-          home: RecipeDetailScreen(recipe: firstRecipe),
+          home: RecipeListScreen(),
     );
   }
 }
